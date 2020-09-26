244903394fa2fef0c6cbac143d744772b0a395e9
Djaysson <djayson.dev@gmail.com>
2020-09-25 22:31:15 -0300

    Finalizing the connection of the api to the project frontend

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -1,32 +1,53 @@
 import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+import api from '../../services/api';
 
 import './styles.css';
 
-function TeacherItem() {
+export interface Teacher{
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+
+}
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+ function createNewConnection(){
+   api.post('connections', {
+     user_id: teacher.id
+   })
+ }
+
   return (
     <article className="teacher-item">
       <header>
-        <img src="https://avatars1.githubusercontent.com/u/47895442?s=460&u=96ad5363dcd9963dedd0bccd23a9dafea6a3d38d&v=4" alt="Djayson Rodrigues" />
+        <img src={teacher.avatar} alt={teacher.name} />
         <div>
-          <strong>Djayson Rodrigues</strong>
-          <span>Quimica</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
-      </header>
-      <p>
-        Entusiasta das melhores tecnologias de química avançada.
-            <br /><br />
-            Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
-          </p>
+      </header> 
+      <p>{teacher.bio}</p>
       <footer>
         <p>
           Preço/hora
-              <strong>R$ 80,00</strong>
+              <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type="button">
+        <a 
+        target="_blank"
+        onClick={createNewConnection} 
+        href={`https://wa.me/${teacher.whatsapp}`}>
+
           <img src={whatsappIcon} alt="Whatsapp" />
               Entrar em contato
-            </button>
+            </a>
       </footer>
     </article>
   )
